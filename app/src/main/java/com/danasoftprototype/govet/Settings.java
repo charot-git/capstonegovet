8353a6521f273e04bc5a7259123a566c3beb1d5b
charot-git <112631097+charot-git@users.noreply.github.com>
2022-10-10 21:11:59 +0800

    settings content

--- app/src/main/java/com/danasoftprototype/govet/Settings.java
+++ app/src/main/java/com/danasoftprototype/govet/Settings.java
@@ -1,18 +1,26 @@
 package com.danasoftprototype.govet;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Settings#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class Settings extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -24,8 +32,12 @@ public class Settings extends Fragment {
     private String mParam1;
     private String mParam2;
 
+
+
     public Settings() {
         // Required empty public constructor
+
+
     }
 
     /**
@@ -60,5 +72,58 @@ public class Settings extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView account = (TextView) view.findViewById(R.id.accountview);
+        TextView display = (TextView) view.findViewById(R.id.displayview);
+        TextView privacyterms = (TextView) view.findViewById(R.id.privacyandtermsview);
+        TextView aboutus = (TextView) view.findViewById(R.id.aboutusview);
+        TextView help = (TextView) view.findViewById(R.id.helpview);
+        TextView logout = (TextView) view.findViewById(R.id.logoutview);
+
+        account.setOnClickListener(this::accountmethod);
+        display.setOnClickListener(this::displaymethod);
+        privacyterms.setOnClickListener(this::privacytermsmethod);
+        aboutus.setOnClickListener(this::aboutusmethod);
+        help.setOnClickListener(this::helpmethod);
+        logout.setOnClickListener(this::logoutmethod);
+    }
+
+
+    private void logoutmethod(View view){
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(getActivity().getApplication(), "Logout successful", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getActivity().getApplication(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void helpmethod(View view){
+        Intent intent = new Intent(getActivity().getApplication(),help.class);
+        startActivity(intent);
+    }
+
+    private void aboutusmethod(View view) {
+        Intent intent = new Intent(getActivity().getApplication(),aboutus.class);
+        startActivity(intent);
+    }
+
+    private void privacytermsmethod(View view) {
+        Intent intent = new Intent(getActivity().getApplication(),privacyterm.class);
+        startActivity(intent);
+    }
+
+    private void displaymethod(View view) {
+        Intent intent = new Intent(getActivity().getApplication(), display.class);
+        startActivity(intent);
+    }
+
+    private void accountmethod(View view) {
+        Intent intent = new Intent(getActivity().getApplication(), account.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/danasoftprototype/govet/account.java
@@ -0,0 +1,14 @@
+package com.danasoftprototype.govet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
