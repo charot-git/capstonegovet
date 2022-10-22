e33d7240b69c9c0bb3595371b2dc605db3da69ba
LiwanagDale <111510725+LiwanagDale@users.noreply.github.com>
2022-10-22 15:49:57 +0800

    Layouts Vet Admin not final Add friend Layout

---
+++ app/src/main/java/com/danasoftprototype/govet/FrontEnd/AddFriend.java
@@ -0,0 +1,6 @@
+package com.danasoftprototype.govet.FrontEnd;
+
+import android.app.Activity;
+
+public class AddFriend extends Activity {
+}
---
+++ app/src/main/java/com/danasoftprototype/govet/FrontEndVet/vetUsers.java
@@ -0,0 +1,16 @@
+package com.danasoftprototype.govet.FrontEndVet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.danasoftprototype.govet.R;
+
+public class vetUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vet_users);
+    }
+}
