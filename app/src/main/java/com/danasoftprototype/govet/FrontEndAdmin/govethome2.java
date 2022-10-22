afe243978ad2f335e86245c5f340c77d3519a2d7
charot-git <112631097+charot-git@users.noreply.github.com>
2022-10-22 16:20:46 +0800

    admin home fix

--- app/src/main/java/com/danasoftprototype/govet/FrontEndAdmin/govethome2.java
+++ app/src/main/java/com/danasoftprototype/govet/FrontEndAdmin/govethome2.java
@@ -1,13 +1,18 @@
 package com.danasoftprototype.govet.FrontEndAdmin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ImageView;
 
 import com.danasoftprototype.govet.R;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,8 +24,8 @@ import com.danasoftprototype.govet.databinding.ActivityGovethome2Binding;
 
 public class govethome2 extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
     private ActivityGovethome2Binding binding;
+    ImageView drawerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,42 +34,46 @@ public class govethome2 extends AppCompatActivity {
         binding = ActivityGovethome2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarGovethome2.toolbar);
-        binding.appBarGovethome2.fab.setOnClickListener(new View.OnClickListener() {
+        NavigationView navigationView = (binding.navView);
+        DrawerLayout drawerLayout = (binding.drawerLayout);
+        drawerButton = (binding.hamburger);
+
+        drawerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home,
-                R.id.nav_adminUsers,
-                R.id.nav_adminAppointment,
-                R.id.nav_adminOnlinePayment,
-                R.id.nav_adminSettings)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_govethome2);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.govethome2, menu);
-        return true;
-    }
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        item.setChecked(true);
+                        finishAfterTransition();
+                        break;
+                    case R.id.nav_adminUsers:
+                        startActivity(new Intent(getApplication(),adminUsers.class));
+                        finishAfterTransition();
+                        break;
+                    case R.id.nav_adminAppointment:
+                        startActivity(new Intent(getApplication(),adminAppoinment.class));
+                        finishAfterTransition();
+                        break;
+                    case R.id.nav_adminOnlinePayment:
+                        startActivity(new Intent(getApplication(),adminOnlinePayments.class));
+                        finishAfterTransition();
+                        break;
+                    case R.id.nav_adminSettings:
+                        startActivity(new Intent(getApplication(),adminSettings.class));
+                        finishAfterTransition();
+                        break;
+                }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_govethome2);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+                return true;
+            }
+        });
+
     }
 }
