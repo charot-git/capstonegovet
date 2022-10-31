b204b85a6c794db012bac86a858c04500db9b1a8
LiwanagDale <111510725+LiwanagDale@users.noreply.github.com>
2022-10-31 14:41:46 +0800

    pets monitoring retouch others xml

--- app/src/main/java/com/danasoftprototype/govet/FrontEndVet/vet_monitor_update.java
+++ app/src/main/java/com/danasoftprototype/govet/FrontEndVet/vet_monitor_update.java
@@ -11,6 +11,6 @@ public class vet_monitor_update extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vet_monitor_update);
+        setContentView(R.layout.activity_monitor_update);
     }
 }
