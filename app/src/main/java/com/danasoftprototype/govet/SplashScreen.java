af3e950eb5d3c9cc097f664d73d6955148b66874
LiwanagDale <111510725+LiwanagDale@users.noreply.github.com>
2022-11-04 22:42:36 +0800

    cleaning 2

--- app/src/main/java/com/danasoftprototype/govet/SplashScreen.java
+++ app/src/main/java/com/danasoftprototype/govet/SplashScreen.java
@@ -17,6 +17,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(SplashScreen.this,MainActivity.class));
                 finish();
             }
-        },2000)
+        },1000)
 ;    }
 }
