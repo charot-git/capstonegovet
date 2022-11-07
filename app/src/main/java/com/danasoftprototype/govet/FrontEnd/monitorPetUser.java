9b72676277039dec3ff48502112982b789fb9f1b
charot-git <112631097+charot-git@users.noreply.github.com>
2022-11-08 02:44:10 +0800

    buttons monitoring contact and history

--- app/src/main/java/com/danasoftprototype/govet/FrontEnd/monitorPetUser.java
+++ app/src/main/java/com/danasoftprototype/govet/FrontEnd/monitorPetUser.java
@@ -1,7 +1,9 @@
 package com.danasoftprototype.govet.FrontEnd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +13,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.danasoftprototype.govet.R;
+import com.danasoftprototype.govet.UserLogs;
+import com.danasoftprototype.govet.chatActivity;
 import com.danasoftprototype.govet.databinding.ActivityMonitorPetUserBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 public class monitorPetUser extends AppCompatActivity {
@@ -19,6 +29,8 @@ public class monitorPetUser extends AppCompatActivity {
     ImageView dp,back;
     TextView name, breed, status, date, time;
     Button contact, history;
+
+    String userName, userUsername, userImage, userEmail, imageOfCurrentUser;
 
     ActivityMonitorPetUserBinding binding;
 
@@ -63,14 +75,47 @@ public class monitorPetUser extends AppCompatActivity {
         contact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(monitorPetUser.this, "contact", Toast.LENGTH_SHORT).show();
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+                databaseReference.child("6UAPpqZQCPUSlxaLYcWNOBq4pnm1").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        DataSnapshot dataSnapshot = task.getResult();
+
+                        userName = String.valueOf(dataSnapshot.child("name").getValue());
+                        userUsername = String.valueOf(dataSnapshot.child("username").getValue());
+                        userImage = String.valueOf(dataSnapshot.child("image").getValue());
+                        userEmail = String.valueOf(dataSnapshot.child("email").getValue());
+
+                        DatabaseReference currentUser = FirebaseDatabase.getInstance().getReference("Users");
+                        currentUser.child(FirebaseAuth.getInstance().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                DataSnapshot dataSnapshot = task.getResult();
+                                imageOfCurrentUser = String.valueOf(dataSnapshot.child("image").getValue());
+
+                                Intent intent = new Intent(getApplication(), chatActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                        .putExtra("name_of_roomate" , userName)
+                                        .putExtra("username_of_roomate", userUsername)
+                                        .putExtra("dp_of_roomate", userImage)
+                                        .putExtra("email_of_roomate", userEmail)
+                                        .putExtra("dp_of_user", imageOfCurrentUser);
+
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        });
+
+                    }
+                });
             }
         });
 
         history.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(monitorPetUser.this, "history", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplication(), UserLogs.class));
+                finish();
             }
         });
 
