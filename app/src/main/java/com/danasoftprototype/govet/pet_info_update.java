7394c9c4d570ece160a47b80b69976c66ecd7aac
LiwanagDale <111510725+LiwanagDale@users.noreply.github.com>
2022-11-07 16:18:12 +0800

    appointment confirm update info pet

---
+++ app/src/main/java/com/danasoftprototype/govet/pet_info_update.java
@@ -0,0 +1,14 @@
+package com.danasoftprototype.govet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pet_info_update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_info_update);
+    }
+}
